78bcf3b6707056561da9ba5cd3172cfd9d220cc6
dwallin33 <dwallin33@354aca26-de27-0410-a198-a53a36578689>
2007-06-09 13:52:13 +0000

    size of blobs for config app was wrong

--- multitouch/src/configapp.cpp
+++ multitouch/src/configapp.cpp
@@ -64,7 +64,8 @@ public:
 	TouchData data;
 	void draw()
 	{
-		float rad = (data.height + data.width) / 2.0;
+		float rad = 0.05;
+			//(data.height + data.width) / 2.0;
 
 		float X = (data.X*2.0f) - 1.0;
 		float Y = ((1.0-data.Y)*2.0f) - 1.0;
